61857423922aa8ec3a2406524834c3cbdb0c9176
Matt Matheus <matt.matheus@outlook.com>
2024-01-06 14:17:31 -0800

    Implement raw endpoint

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+from bson import json_util
 from pymongo import MongoClient
 
 app = FastAPI()
@@ -13,12 +15,17 @@ def get_data_from_db():
     data = list(collection.find({}))
     return data
 
-
-
 @app.get("/")
 async def read_root(request: Request):
     data = get_data_from_db()
     return templates.TemplateResponse("table.html", {"request": request, "data": data})
 
+@app.get("/raw")
+async def return_raw():
+    data = get_data_from_db()
+    json_compatible_data = json_util.dumps(data)
+    return JSONResponse(content=json_compatible_data)
+    
+# This is to allow referencing the styles.css in our static directory
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
