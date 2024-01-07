e2ecff69368318b15e4edcfbcda199c9da0fbd8b
Matt Matheus <matt.matheus@outlook.com>
2024-01-06 20:39:11 -0800

    resetbuttons!

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, RedirectResponse
 from bson import json_util
 from pymongo import MongoClient
 
@@ -44,9 +44,37 @@ async def create_dbentry():
     return status.HTTP_201_CREATED
 
 
-@app.post("/reset")
+@app.post("/createmany")
+async def create_item(count: int = 1):
+    if count < 1:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Repeat count must be at least 1.",
+        )
+    try:
+        for _ in range(count):
+            messagestack = azuremessage_pb2.AzureMessageStack()
+            messageobj = createstack.GenerateUserMessageObject(messagestack)
+            for azuremessage in messageobj.azuremessage:
+                dbmessage = createstack.ConvertStackToDict(azuremessage)
+                collection.insert_one(dbmessage)
+        return {
+            "message": f"Item created successfully {count} times"
+        }, status.HTTP_202_ACCEPTED
+    except Exception as e:
+        raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@app.get("/reset")
 async def reset_db():
     collection.delete_many({})
+    response = RedirectResponse(url="/")
+    return response
+
+
+@app.post("/purgezeros")
+async def purge_zeros():
+    collection.delete_many({"id": 0})
     return status.HTTP_202_ACCEPTED
 
 
